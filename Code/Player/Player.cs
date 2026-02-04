8575d9d6ba8a000d3f938cdbb9aa0ef2cdca8793
Ser0the <serg0theguy@gmail.com>
2026-02-04 23:21:58 +0300

    Player system basics

---
+++ Code/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using Sandbox;
+using System;
+
+public class Player
+{
+	public PlayerComponent ComponentInstance { get; set; }
+
+	public Connection PlayerConnection
+	{
+		get => Connection.Find( ComponentInstance.ConnectionID );
+		set => ComponentInstance.ConnectionID = value.Id;
+	}
+
+	public static List<Player> Players { get; private set; }
+
+	public Player( PlayerComponent component )
+	{
+		ComponentInstance = component;
+		Players.Add( this );
+	}
+}
+
+public class PlayerComponent : Component
+{
+	[Sync]
+	[Property, ReadOnly]
+	public Guid ConnectionID { get; set; } = Guid.Empty;
+	public Player PlayerInstance { get; set; } = null;
+
+	protected override void OnStart()
+	{
+		Player Player = new Player( this );
+		this.PlayerInstance = Player;
+	}
+}
--- Code/Player/PlayerManager.cs
+++ Code/Player/PlayerManager.cs
@@ -1,2 +1,41 @@
 using Sandbox;
+using System.Linq;
+using System.Threading.Channels;
 
+public class PlayerManager : Component, Component.INetworkListener
+{
+
+	void INetworkListener.OnConnected( Connection channel )
+	{
+		GameObject PlayerObject = new GameObject(this.GameObject);
+		PlayerComponent PlayerComp = PlayerObject.AddComponent<PlayerComponent>();
+
+		PlayerComp.ConnectionID = channel.Id;
+
+		PlayerObject.NetworkSpawn();
+	}
+
+	protected override void OnEnabled()
+	{
+		if (IsProxy) { return; }
+		List<Connection> fullConnections = [];
+
+		foreach ( PlayerComponent component in Scene.GetAllComponents<PlayerComponent>() )
+		{
+			fullConnections.Add(Connection.Find(component.ConnectionID));
+		}
+
+		foreach (Connection connection in Connection.All )
+		{
+			if (!fullConnections.Contains(connection) )
+			{
+				GameObject PlayerObject = new GameObject( this.GameObject );
+				PlayerComponent PlayerComp = PlayerObject.AddComponent<PlayerComponent>();
+
+				PlayerComp.ConnectionID = connection.Id;
+
+				PlayerObject.NetworkSpawn();
+			}
+		}
+	}
+}
